f69d5db1a87f67612c8cfd2e26cc75cfe7df9be2
Unknown <pshenychnyi96@gmail.com>
2018-02-07 23:26:35 +0200

    Branch test
    Dummy commit

---
+++ vlog_import_tests/sources/tests/vlog_import_dataflow_process_test.cpp
@@ -0,0 +1,15 @@
+#include "stdafx.h"
+
+#include "vlog_import_tests\catch.hpp"
+#include "vlog_import_tests\sources\fixtures\vlog_import_test_variables_fixture.hpp"
+
+/***************************************************************************/
+
+namespace VlogModelImportTests {
+
+/***************************************************************************/
+
+
+/***************************************************************************/
+
+}
